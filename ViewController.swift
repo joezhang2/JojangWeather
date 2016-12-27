16d00168115804ff55f9eb99e7f18bf85e272367
Joe Zhang <joe.zhang2@gmail.com>
2016-12-26 21:45:22 -0600

    got location services working

--- ViewController.swift
+++ ViewController.swift
@@ -7,30 +7,104 @@
 //
 
 import Cocoa
+import CoreLocation
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var cityTextField: NSTextField!
+    
+    @IBOutlet weak var cityLabel: NSTextField!
+    
+    lazy var geocoder = CLGeocoder()
+    let locationManager = CLLocationManager()
+    
+    @IBAction func updateWeather(_ sender: Any) {
+        var currentLocation: CLLocation!
+        currentLocation = locationManager.location
+        
+        let text1: String = "\(currentLocation.coordinate.longitude)"
+        let text2: String = "\(currentLocation.coordinate.latitude)"
+        print(text2)
+        print(text1)
+        print("got location")
 
+    }
     
-    @IBOutlet weak var locationLabel: NSTextFieldCell!
-    
-    @IBOutlet weak var locationValue: NSTextField!
-    
-    @IBAction func locationSetButton(_ sender: Any) {
-        locationLabel.stringValue = locationValue.stringValue
+    @IBAction func autoSetLocation(_ sender: Any) {
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if (CLLocationManager.locationServicesEnabled()){
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyThreeKilometers
+            
+            locationManager.startUpdatingLocation()
+            locationManager.stopUpdatingLocation()
+        }
+    }
+    
+    
+    @IBAction func handleCity(_ sender: AnyObject) {
+        //cityLabel.stringValue = cityTextField.stringValue
+        
+        let locationData = cityTextField.stringValue.components(separatedBy: ",")
+        
+        let country:String = "USA"
+        let city: String = locationData[0]
+        let state: String = locationData[1]
+        
+        let address = "\(country), \(state), \(city)"
+        
+        // Geocode Address String
+        geocoder.geocodeAddressString(address) { (placemarks, error) in
+            // Process Response
+            self.processResponse(withPlacemarks: placemarks, error: error)
+        }
+        
+    }
+    
+    
+    private func processResponse(withPlacemarks placemarks: [CLPlacemark]?, error: Error?) {
+        // Update View
+        //geocodeButton.isHidden = false
+        //activityIndicatorView.stopAnimating()
+        
+        if let error = error {
+            print("Unable to Forward Geocode Address (\(error))")
+            cityLabel.stringValue = "Unable to Find Location for Address"
+            
+        } else {
+            print("hi")
+            var location: CLLocation?
+            
+            if let placemarks = placemarks, placemarks.count > 0 {
+                location = placemarks.first?.location
+            }
+            
+            if let location = location {
+                let coordinate = location.coordinate
+                cityLabel.stringValue = "\(coordinate.latitude), \(coordinate.longitude)"
+            } else {
+                cityLabel.stringValue = "No Matching Location Found"
+            }
+        }
+    }
+    
+
+    /*
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
+    }*/
 
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
-
-
+    
 }
 
--- WeatherData.swift
+++ WeatherData.swift
@@ -7,10 +7,12 @@
 //
 
 import Cocoa
-import Foundation
+import CoreLocation
 
 
-struct Forecast {
+let owmKey: String = "5b9d4a06a07a8a29f234cb9dd91cb2c4"
+
+struct Weather {
     var weatherCondition: String
     var temperature: Double
     
@@ -20,21 +22,87 @@ struct Forecast {
     }
 }
 
-struct WeatherData {
-    var fiveDayForecast: [Forecast] = [Forecast](repeating: Forecast(weatherCondition: "Null", temperature: 0.0), count: 5 )
+struct Forecast {
+    var fiveDayForecast: [Weather]
+    
+    init() {
+        fiveDayForecast = [Weather](repeating: Weather(weatherCondition: "Null", temperature: 0.0), count: 5 )
+    }
     
     mutating func setForecast(conditions: [String], temperature: [Double]) {
         fiveDayForecast.removeAll()
-        var day: Forecast = Forecast(weatherCondition: "null", temperature: 0.0)
+        var day: Weather = Weather(weatherCondition: "null", temperature: 0.0)
         for (cond, temp) in zip(conditions, temperature) {
-            day = Forecast(weatherCondition: cond, temperature: temp)
+            day = Weather(weatherCondition: cond, temperature: temp)
             fiveDayForecast.append(day)
         }
     }
+    
 }
 
-/*
-class WeatherData: NSObject {
 
+class WeatherForecast: NSObject {
+    var curData = Forecast()
+    lazy var geocoder = CLGeocoder()
+    
+    override init(){
+        
+    }
+    
+
+    /*
+    func getLocation() -> CLLocation{
+        
+        let locationManager = CLLocationManager()
+        var currentLocation: CLLocation!
+        
+        let authStatus: CLAuthorizationStatus = CLLocationManager.authorizationStatus()
+        
+        if authStatus == CLAuthorizationStatus.restricted || authStatus == CLAuthorizationStatus.denied {
+            print("Sorry, location is not permited, you need to enable it in System Preferences")
+            exit(1)
+        }
+        
+        if CLLocationManager.locationServicesEnabled() != true {
+            print("Sorry, location not supported")
+            exit(2)
+        }
+        locationManager.startUpdatingLocation()
+        
+        currentLocation = locationManager.location
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.stopUpdatingLocation()
+        
+        let text1: String = "\(currentLocation.coordinate.longitude)"
+        let text2: String = "\(currentLocation.coordinate.latitude)"
+        print(text1)
+        print(text2)
+        return currentLocation
+        
+    }*/
+    /*
+    func lookUpCity(){
+        /*
+        var jsonData: Data?
+        
+        if let file = Bundle.main.path(forResource: "city.list.us", ofType: "json") {
+            jsonData = try? Data(contentsOf: URL(fileURLWithPath: file))
+        } else {
+            print("Fail")
+        }
+        let jsonString = String(data: jsonData!, encoding: .utf8)
+        let dataFromString = jsonString?.data(using: .utf8)
+        let json3 = JSON(data: dataFromString!)
+        print("jsonData:\(json3)")
+        */
+        //let location: CLLocation! = getLocation()
+        let text1: String = "\(location.coordinate.longitude)"
+        let text2: String = "\(location.coordinate.latitude)"
+        print(text1)
+        print(text2)
+        
+    }*/
 }
-*/
+
